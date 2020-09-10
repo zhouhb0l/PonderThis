4b5b2f57dae63d2c330084325fec46b111779e9a
lengshi01 <lengshi01@users.noreply.github.com>
2020-09-10 23:11:08 +0800

    get profile

--- Sep/findAm.py
+++ Sep/findAm.py
@@ -1,3 +1,4 @@
+#import numpy as np
 dataset = [
     111100001110001101011101110111000000, 111100001011010110101001111011100101,
     111100001110010101101110110111100101, 111100001111000101011101110111100101,
@@ -30,17 +31,49 @@ def generate3cycleSet(RelationMap):
     for i in range(dimension):
         for j in range(i):
             for k in range(j):
-                if(RelationMap[i][j]==RelationMap[j][k] and RelationMap[j][k]==RelationMap[k][i]):
+                if RelationMap[i][j]==1 and RelationMap[j][k]==1 and RelationMap[k][i]==1:
                     cycleSet.append([i,j,k])
+                if RelationMap[i][j]==0 and RelationMap[j][k]==0 and RelationMap[k][i]==0:
+                    cycleSet.append([k,j,i])
     return cycleSet
 
-def generateProfile(RelationMap):
-    dimension=len(RelationMap)
-    return 1
 
-RelationMap=generateRelationMap(dataset[1],9)
-cycleSet=generate3cycleSet(RelationMap)
-print(cycleSet)
-#print(generateProfile(RelationMap))
+def generateEdgeProfile(cycleSet,dimension):
+    edgeProfile=[[0 for x in range(dimension)] for x in range(dimension)]
+    for cycle in cycleSet:
+        edgeProfile[cycle[1]][cycle[0]]+=1
+        edgeProfile[cycle[2]][cycle[1]]+=1
+        edgeProfile[cycle[2]][cycle[0]]+=1
+        
+        edgeProfile[cycle[0]][cycle[1]]+=1
+        edgeProfile[cycle[1]][cycle[2]]+=1
+        edgeProfile[cycle[0]][cycle[2]]+=1
+    return edgeProfile
+
+def generateProfile(cycleSet,edgeProfile):
+    profile=[]
+    dimension=len(edgeProfile)
+    for i in range(dimension):
+        profileOfVertice=[]
+        for cycle in cycleSet:
+            if i in cycle:
+                ind=cycle.index(i)
+                np=cycle[(ind+1)%3]
+                pp=cycle[(ind+2)%3]
+                profileOfVertice.append([edgeProfile[i][np],edgeProfile[np][pp],edgeProfile[pp][i]])
+        profile.append(profileOfVertice)
+    return profile
+
+for i in range(15):
+  RelationMap=generateRelationMap(dataset[i],9)
+  cycleSet=generate3cycleSet(RelationMap)
+  print(len(cycleSet))
+  edgeProfile=generateEdgeProfile(cycleSet,len(RelationMap))
+  #print(edgeProfile)
+  profile=generateProfile(cycleSet,edgeProfile)
+
+  for vertice in profile:
+    print(vertice)
+  print("next one")
 
 
